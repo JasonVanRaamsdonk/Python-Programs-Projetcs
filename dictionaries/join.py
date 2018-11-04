d157c4f33e7ac13aa876f6c27e95b611d10ac230
Jason van Raamsdonk <C17764691@mydit.ie>
2018-11-04 22:43:01 +0000

    Add files via upload

---
+++ dictionaries/join.py
@@ -0,0 +1,11 @@
+
+my_list = ["a", "b", "c", "d"]
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
+
+newString = ''
+for c in my_list:
+    # newString += c + ", "
+    newString = "mississippi ".join(numbers)
+
+print(newString)
